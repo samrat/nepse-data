5267b5aed09f6c4a61035a5a4f0d755f4140bb1a
Samrat Man Singh <samratmansingh@gmail.com>
2013-06-27 12:59:36 +0545

    Add market-open? and make stock-details functional.

--- src/nepse_data/core.clj
+++ src/nepse_data/core.clj
@@ -4,6 +4,14 @@
 
 (def latest-share-url "http://nepalstock.com/datanepse/index.php")
 
+(defn market-open? []
+  (let [html (l/parse (:body (http/get latest-share-url)))
+        marketcl (l/select html
+                           (l/class= "marketcl"))]
+    (if (empty? marketcl)
+      true
+      false)))
+
 (defn all-traded []
   (let [html (l/parse (:body (http/get latest-share-url)))
         title-row (-> html
@@ -62,6 +70,15 @@
       (#(map :content %))
       (#(map last %))))
 
+(defn parse-rupees
+  "Returns the extracted numerical value from a string in the format Rs.50 or Rs. 50."
+  [string]
+  (-> (clojure.string/split string #"Rs. *")
+      second
+      (clojure.string/replace #"," "")
+      read-string
+      int))
+
 (defn stock-details [stock-symbol]
   (let [base-url "http://nepalstock.com/companydetail.php?StockSymbol=%s"
         stock-page (l/parse (:body (http/get (format base-url stock-symbol))))
@@ -69,14 +86,47 @@
                         (l/select (l/class= "dataTable"))
                         first
                         l/zip
-                        (l/select (l/class= "row1")))
-        first-table-vals (tr-values first-table)]
+                        (l/select (l/class= "row1"))
+                        first)
+        first-table-vals (tr-values first-table)
+        second-table (-> stock-page
+                        (l/select (l/class= "dataTable"))
+                        second
+                        l/zip
+                        (l/select (l/class= "row1"))
+                        first)
+        second-table-vals (tr-values second-table)]
+    (prn second-table-vals)
     {:last-traded (first first-table-vals)
-     :last-trade-price (-> (second first-table-vals)
-                           (clojure.string/split #"Rs. ")
-                           second)
-     :net-change-in-rs (-> (nth first-))}
-    ))
+     :last-trade-price (Integer/parseInt (second first-table-vals))
+     :net-change-in-rs (-> first-table-vals
+                           (nth 2)
+                           parse-rupees)
+     :percent-change (-> first-table-vals
+                         (nth 3)
+                         read-string)
+     :high (-> first-table-vals
+               (nth 4)
+               parse-rupees)
+     :low (-> first-table-vals
+              (nth 5)
+              parse-rupees)
+     :previous-close (-> first-table-vals
+                         (nth 6)
+                         parse-rupees)
+     :symbol (last first-table-vals)
+     :listed-shares (-> (first second-table-vals)
+                        (clojure.string/replace #"," "")
+                        read-string)
+     :paid-up-value (-> (second second-table-vals)
+                        parse-rupees)
+     :total-paid-up-value (-> (nth second-table-vals 2)
+                              parse-rupees)
+     :closing-market-price (-> (nth second-table-vals 3)
+                               parse-rupees)
+     :market-capitalization (-> (nth second-table-vals 4)
+                                parse-rupees)
+     :market-capitalization-date (last second-table-vals)}))
 
 (defn foo
   "I don't do a whole lot."
