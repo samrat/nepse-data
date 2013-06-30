b583c03832c1b80861fc6da356a8df602c093d07
Samrat Man Singh <samratmansingh@gmail.com>
2013-06-30 12:57:56 +0545

    Add utils_test.clj.

---
+++ test/nepse_data/utils_test.clj
@@ -0,0 +1,10 @@
+(ns nepse-data.utils-test
+  (:use clojure.test
+        nepse-data.utils))
+
+(deftest parse-string-test
+  (is (= (parse-string "Rs. 50") 50))
+  (is (= (parse-string "24.5%") 24.5))
+  (is (= (parse-string "123,456,789" 123456789)))
+  (is (= (parse-string "ACEDBL") "ACEDBL"))
+  (is (= (parse-string "2013-05-16") "2013-05-16")))
