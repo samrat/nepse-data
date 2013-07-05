a31b0b991bb8026d0789b859c35ac6a83fc7cb61
Samrat Man Singh <samratmansingh@gmail.com>
2013-07-05 09:47:27 +0545

    More logging.

--- src/nepse_data/web.clj
+++ src/nepse_data/web.clj
@@ -3,6 +3,7 @@
         ring.middleware.json
         ring.middleware.cors
         ring.util.response
+        [clojure.tools.logging :only [info]]
         [org.httpkit.server :only [run-server]])
   (:require [compojure.route :as route]
             [compojure.handler :as handler]
@@ -22,13 +23,23 @@
   ;;(route/not-found (layout/four-oh-four))
   )
 
+(defn- wrap-request-logging [handler]
+  (fn [{:keys [request-method uri] :as req}]
+    (let [resp (handler req)]
+      (info (name request-method) (:status resp)
+            (if-let [qs (:query-string req)]
+              (str uri "?" qs) uri))
+      resp)))
+
 (def application (-> (handler/site routes)
                      wrap-json-response
                      reload/wrap-reload
+                     wrap-request-logging
                      (wrap-cors
                       :access-control-allow-origin #".+")))
 
 (defn -main [& args]
   (let [port (Integer/parseInt 
                (or (System/getenv "PORT") "8080"))]
-    (run-server application {:port port :join? false})))
+    (run-server application {:port port :join? false})
+    (info "Server started. Listening on port " port)))
